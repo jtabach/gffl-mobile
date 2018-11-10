ed5b7871a2a0132cfac84971faa45ecec753a6d7
jtabach <jeff.n.tabachnick@gmail.com>
2018-11-10 11:03:42 +0700

    test query to backend server

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
+import * as Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+const { manifest } = Expo.Constants;
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const devUrl = manifest.debuggerHost.split(`:`).shift().concat(`:5000`);
+    axios.get(`http://${devUrl}`)
+    .then(res => console.log(res.data));
+  }
+
   render() {
     return (
       <View style={styles.container}>
